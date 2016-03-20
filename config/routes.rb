ab0b370f02b2ce34fea273b9c433925a0a4591ce
Eric Guo <eric.guo@sandisk.com>
2016-03-20 23:26:46 +0800

    After running rake rails:update

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
-EmberTutorial::Application.routes.draw do
+Rails.application.routes.draw do
+  # The priority is based upon order of creation: first created -> highest priority.
+  # See how all your routes lay out with "rake routes".
   root to: 'home#index'
 
   get 'chapters', to: 'home#chapters'
